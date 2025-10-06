fb58bafd7489c291566c36351538a6b5d61fe0d4
Guilherme <116519824+gui-ccr@users.noreply.github.com>
2025-10-06 00:04:53 -0300

    feat(footer) adicione o link de whatsapp no footer - atualiza tambem o hover para que a cor represente a plataforma social

--- meu-portfolio/src/Components/Footer/Footer.tsx
+++ meu-portfolio/src/Components/Footer/Footer.tsx
@@ -13,6 +13,7 @@ const currentYear = new Date().getFullYear();
       <div className="footer-socials">
         <a href="https://www.linkedin.com/in/gui-ccr-"  target="_blank" className="social-link-footer">LinkedIn</a>
         <a href="https://github.com/gui-ccr"  target="_blank" className="social-link-footer">GitHub</a>
+        <a href="https://wa.me/5533998217341"  target="_blank" className="social-link-footer" id='whatsapp'>WhatsApp</a>
       </div>
     </footer>
   );
